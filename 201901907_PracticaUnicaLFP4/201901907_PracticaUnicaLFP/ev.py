6ea1cc596297245020ca6773827e7f142599afd1
Byron-Estuardo <69336924+Byron-Estuardo@users.noreply.github.com>
2020-08-30 17:35:20 -0600

    Arreglo de Funciones, salida

---
+++ 201901907_PracticaUnicaLFP4/201901907_PracticaUnicaLFP/ev.py
@@ -0,0 +1,39 @@
+import json
+import re
+
+lista = []
+listAux = []
+
+class texto:
+    def __init__(self, nombre, edad, activo, promedio):
+        self.nombre = nombre
+        self.edad = edad
+        self.activo = activo
+        self.promedio = promedio
+
+def subirArch:
+    try:
+        nom = (input("~Ingrese la Instruccion: "))
+        Buscar1 = re.search('CARGAR', nom.upper())
+        InstruccionInicial = nom[int(Buscar1.start()):int(Buscar1.end())]
+        Comparacion = str(InstruccionInicial)
+        RestoEn = nom[Buscar1.end():len(nom)]
+        ArchivosJSON = re.sub('\s', '', RestoEn)
+        ArchivosJSON1 = re.split(',', ArchivosJSON)
+        if 'CARGAR' == Comparacion.upper():
+            for i in ArchivosJSON1:
+                with open(ArchivosJSON1) as FileU:
+                    reader = json.load(FileU)
+
+    except:
+        print('F')
+
+for z in range(0, len(ArchivosFin)):
+                        A = ArchivosFin[z]['nombre']
+                        B = ArchivosFin[z]['edad']
+                        C = ArchivosFin[z]['activo']
+                        D = ArchivosFin[z]['promedio']
+                        print(A)
+                        print(B)
+                        print(C)
+                        print(D)
